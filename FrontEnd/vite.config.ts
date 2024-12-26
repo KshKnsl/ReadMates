3984121d44979bad8856466f15b6d00d0cd8d378
KushKansal <mrkushkansal@gmail.com>
2024-12-26 13:10:22 +0530

    Add sitemap URL to robots.txt and include '/create' route in sitemap plugin configuration

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import sitemap from 'vite-plugin-sitemap';
-
 export default defineConfig({
   plugins: [react(), 
     sitemap({
@@ -10,6 +9,7 @@ export default defineConfig({
         '/',
         '/profice',
         '/articles',
+        '/create',
       ]
     })
   ],
