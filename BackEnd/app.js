8dc40d48c21c3b94094367c6a78bf5338bbc5de0
KushKansal <mrkushkansal@gmail.com>
2024-12-30 21:07:37 +0530

    CORS issue Fixed

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -9,10 +9,7 @@ const getArticleRoutes = require("./routes/getArticle.routes");
 const colaborationRoutes = require("./routes/colab.route");
 const {protect}= require("./middlewares/authMiddleware");
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
